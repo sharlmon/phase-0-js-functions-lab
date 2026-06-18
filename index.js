a1c37f183941fcb1b9592a512f5de278b593110c
sharlmon <sharlmon19@gmail.com>
2026-06-19 02:08:20 +0300

    done the exercise on functions

--- index.js
+++ index.js
@@ -3,8 +3,63 @@ function calculateTax(amount) {
   let taxValue = amount * 0.1;
   return taxValue;
 }
-result = calculateTax(10000);
+ result = calculateTax(10000);
+ console.log(result);
+
+
+//function to convert string to upperecase
+function convertToUpperCase(text) {
+  let bigText = text.toUpperCase();
+  return bigText;
+}
+let result = convertToUpperCase("sharlmon");
 console.log(result);
+
+
+//fnction to fid the bigger number
+
+function findMaximum(num1,num2){
+  if (num1 > num2){
+    return num1;
+  }
+  else if (num2 > num1){
+    return num2
+  }
+  else{ 
+    return("they are equal");
+  }
+}
+let result = findMaximum(3,5);
+console.log(result)
+
+
+//function to find out if word is a palindrome
+function isPalindrome(word) {
+  let flippedword = word.split("").reverse().join("");
+  if (word === flippedword) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isPalindrome("sharlmon"));
+console.log(isPalindrome("mom"));
+
+
+//fuction to calculate discounted price
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+ let discountAmount = (discountPercentage /100) * originalPrice ;
+ let buyingPrice = originalPrice - discountAmount ;
+ return buyingPrice;
+}
+
+let result = calculateDiscountedPrice(100,20);
+console.log(result);
+
+
+
+
+
 
 // This is required for the test to function properly
 //module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
