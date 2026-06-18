2b98deb13856c629b7e9603e963ad307feab3c2c
sharlmon <sharlmon19@gmail.com>
2026-06-19 02:24:27 +0300

    made changes and fixed the syntax errors made so that it passes the npm test

--- index.js
+++ index.js
@@ -3,35 +3,29 @@ function calculateTax(amount) {
   let taxValue = amount * 0.1;
   return taxValue;
 }
- result = calculateTax(10000);
- console.log(result);
-
+const taxResult = calculateTax(10000);
+console.log(taxResult);
 
 //function to convert string to upperecase
 function convertToUpperCase(text) {
   let bigText = text.toUpperCase();
   return bigText;
 }
-let result = convertToUpperCase("sharlmon");
-console.log(result);
+const upperResult = convertToUpperCase("sharlmon");
+console.log(upperResult);
 
-
-//fnction to fid the bigger number
-
-function findMaximum(num1,num2){
-  if (num1 > num2){
+//fnction to find the bigger number
+function findMaximum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else {
     return num1;
   }
-  else if (num2 > num1){
-    return num2
-  }
-  else{ 
-    return("they are equal");
-  }
 }
-let result = findMaximum(3,5);
-console.log(result)
-
+const maxResult = findMaximum(3, 5);
+console.log(maxResult);
 
 //function to find out if word is a palindrome
 function isPalindrome(word) {
@@ -45,21 +39,21 @@ function isPalindrome(word) {
 console.log(isPalindrome("sharlmon"));
 console.log(isPalindrome("mom"));
 
-
 //fuction to calculate discounted price
 function calculateDiscountedPrice(originalPrice, discountPercentage) {
- let discountAmount = (discountPercentage /100) * originalPrice ;
- let buyingPrice = originalPrice - discountAmount ;
- return buyingPrice;
+  let discountAmount = (discountPercentage / 100) * originalPrice;
+  let buyingPrice = originalPrice - discountAmount;
+  return buyingPrice;
 }
 
-let result = calculateDiscountedPrice(100,20);
-console.log(result);
-
-
-
-
-
+const discountedResult = calculateDiscountedPrice(100, 20);
+console.log(discountedResult);
 
 // This is required for the test to function properly
-//module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
