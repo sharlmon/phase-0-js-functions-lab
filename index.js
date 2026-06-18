b33c4d120cf0e01723961967eaa3a3bb0fbe61c7
sharlmon <sharlmon19@gmail.com>
2026-06-18 17:15:42 +0300

    added the first function to calculate tax

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//function to calculate taxvalue
+function calculateTax(amount) {
+  let taxValue = amount * 0.1;
+  return taxValue;
+}
+result = calculateTax(10000);
+console.log(result);
 
-
-
-
-
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+// This is required for the test to function properly
+//module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
